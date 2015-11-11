2cc578023e802b37abdd7020a7de3cc7ffafb843
Blake Guilloud <fbguillo@gmail.com>
2015-11-11 10:12:37 -0500

    2nd commit

--- app/arrays.js
+++ app/arrays.js
@@ -15,50 +15,92 @@ exports.arraysAnswers = {
   },
 
   remove : function(arr, item) {
-
+    for(var i = 0; i < arr.length; i++){
+      if(arr[i] === item){
+        arr.splice(i, 1);
+      }
+    }
+    return arr;
   },
 
   removeWithoutCopy : function(arr, item) {
-
+    for(var i = 0; i < arr.length; i++){
+     if(arr[i] === item){
+       arr.splice(i, 1);
+       i--;
+     };
+   };
+   return arr;
   },
 
   append : function(arr, item) {
-
-  },
+      arr.push(item);
+      return arr;
+    },
 
   truncate : function(arr) {
-
+    arr.pop();
+    return arr;
   },
 
   prepend : function(arr, item) {
-
+    arr.unshift(item);
+    return arr;
   },
 
   curtail : function(arr) {
-
+    arr.shift();
+    return arr;
   },
 
   concat : function(arr1, arr2) {
-
+    var combine = arr1.concat(arr2);
+    return combine;
   },
 
   insert : function(arr, item, index) {
-
+    arr.splice(index, 0, item);
+    return arr;
   },
 
   count : function(arr, item) {
-
+    var count = 0;
+    for(var i = 0; i < arr.length; i++){
+      if(arr[i] === item){
+        count++;
+      }
+    }
+    return count;
   },
 
   duplicates : function(arr) {
-
+    var items = {};
+    var duplicates = [];
+    for (var i = 0; i < arr.length; i++) {
+      items[arr[i]] = items[arr[i]] ? items[arr[i]] + 1 : 1; ///dont fully understand, but thanks S.O.
+    };
+    for (var val in items) {
+      if (items[val] > 1) {
+        duplicates.push(+val);
+      };
+    };
+    return duplicates;
   },
 
   square : function(arr) {
-
+    for(var i = 0; i < arr.length; i++){
+     arr[i] *= arr[i];
+   }
+   return arr;
   },
 
   findAllOccurrences : function(arr, target) {
-
-  }
+    var occs =[];
+    for(var i = 0; i < arr.length; i++){
+      if(arr[i] === target){
+        occs.push(i);
+      };
+    };
+      return occs;
+  },
 };
--- app/count.js
+++ app/count.js
@@ -1,7 +1,22 @@
 exports = (typeof window === 'undefined') ? global : window;
 
-exports.countAnswers =  {
+exports.countAnswers = {
   count : function (start, end) {
+    var i;
+    function counter () {
+      console.log(start++);
 
+      if (start <= end) {
+        i = setTimeout(counter, 200);
+      }
+    }
+
+    counter();
+
+    return {
+      cancel : function () {
+        clearTimeout(i);
+      }
+    };
   }
 };
